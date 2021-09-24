03d00a59ec6c6cc3635a76710f7510d7f13efded
marioattieh <mario.attieh.2@gmail.com>
2021-09-24 16:51:25 +0300

    First commit

---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="container-home">
+      <header className="header-home"></header>
+      <body className="body-home">
+        <section className="links-home">
+          <div></div>
+          <div>About Us</div>
+          <div>Projects</div>
+          <div>Media</div>
+          <div>HSE</div>
+          <div>Sponsors</div>
+          <div></div>
+        </section>
+      </body>
+    </div>
+  );
+};
+
+export default Home;
